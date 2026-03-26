be24d70c06173c3a3bcb036d5855d7d0f8bd8438
ms2dwrld <hammedomoniyi3@gmail.com>
2026-03-26 15:02:33 +0100

    Deploy

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,10 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials:true,
+  baseURL: import.meta.env.VITE_API_URL || '',
+  withCredentials: true
 });
-
 
 
 export default api;
