c35b7054443cdceb60a9da4c24a7ae3ad7a87377
ms2dwrld <hammedomoniyi3@gmail.com>
2026-03-27 03:40:30 +0100

    Removed hardcoded google client id from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import {GoogleOAuthProvider} from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='910733132102-aj6ki70ad6a1l3cbb3m0sgvgdmt4clq7.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
